f17b5785bffb2584f5e858c0b6d4b078732ea152
Miguel Sosa <migueljsosaa30@gmail.com>
2022-08-16 17:31:55 -0400

    Eliminando

---
+++ WebApiAutores1/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiAutores1.Entidades;
+
+namespace WebApiAutores1
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Autor> Autores { get; set; }
+        public DbSet<Libro> Libros { get; set; }
+
+    }
+}
---
+++ WebApiAutores1/Controllers/AutoresController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using WebApiAutores1.Entidades;
+
+namespace WebApiAutores1.Controllers
+{
+
+    [ApiController]
+    [Route("Api/autores")]
+    public class AutoresController : ControllerBase
+
+    {
+        private readonly ApplicationDbContext context;
+
+        public AutoresController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Autor>>> Get()
+        {
+
+            return await context.Autores.ToListAsync();
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Autor autor)
+        {
+            context.Add(autor);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpPut("{Id:int}")] 
+        public async Task<ActionResult> Put(Autor autor, int Id)
+        {
+            if (autor.Id != Id)
+            {
+                return BadRequest("El ID del autor no es igual al de la URL");
+            }
+
+            var existe = await context.Autores.AnyAsync(x => x.Id == Id);
+            if (!existe)
+            {
+                return NotFound("No se puede Actualizar porque el registro no existe");
+            }
+
+            context.Update(autor);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpDelete("{Id:int}")]
+        public async Task<ActionResult> Delete(int Id)
+        {
+            var existe = await context.Autores.AnyAsync(x => x.Id == Id);
+            if (!existe)
+            {
+                return NotFound("No se puede eliminar porque el registro no existe");
+            }
+            context.Remove(new Autor { Id = Id });
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
